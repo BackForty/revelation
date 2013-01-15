0be6f0b2ec5bf2ccf6c12bd4ac3b8c1e26db4572
Flip Sasser <flip@x451.com>
2013-01-14 23:13:38 -0500

    Updated command to use local templates

--- lib/revelation/command.rb
+++ lib/revelation/command.rb
@@ -13,32 +13,15 @@ module Revelation
 
     desc "new PRESENTATION_NAME", "Creates a new presentation"
     def new(presentation_name)
-      tmp_checkout = File.join(Dir.tmpdir, "reveal.js.#{Time.now.to_i}")
-      `git clone git://github.com/hakimel/reveal.js.git #{tmp_checkout}`
+      # Set the name up and create the dummy directory
       self.name = presentation_name.underscore
-      directory(
-        "template",
-        name,
-        date: Date.today,
-        name: name
-      )
-      filetype_mappings = {css: "stylesheets", js: "javascripts"}
-      %w(css js).each do |filetype|
-        files = Dir[File.join("#{tmp_checkout}", filetype, "**", "*.#{filetype}")]
-        files.each do |file|
-          file_contents = File.read(file)
-          file = file.gsub(File.join(tmp_checkout, filetype), '')
-          create_file "#{name}/public/#{filetype_mappings[filetype.to_sym]}/#{file}", file_contents
-        end
-      end
-      directory(File.join(tmp_checkout, 'plugin'), "#{name}/public/plugins")
-      FileUtils.rmdir(tmp_checkout)
+      directory("template", name, date: Date.today, name: name)
     end
 
     desc "present", "Start and open a presentation"
     method_option :port, :aliases => ['-p'], :default => 9292, :type => :numeric
     def present
-      pid = fork do
+      fork do
         begin
           `rackup config.ru -p #{options[:port]}`
         rescue SystemExit, Interrupt
@@ -50,6 +33,12 @@ module Revelation
     rescue SystemExit, Interrupt
       warn "\nEnding presentation..."
       exit 1
+    end
+
+    desc "slide SLIDE_NAME", "Add a slide to your deck"
+    def slide(slide_name)
+      create_file "slides/#{slide_name.underscore}.haml"
+      append_file "slides.haml", "= slide :#{slide_name.underscore}"
     end
   end
 end
